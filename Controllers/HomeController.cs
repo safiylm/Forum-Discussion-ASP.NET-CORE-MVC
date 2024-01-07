d8f27ce1399ebd509c2d33da6c78f3bffd18d0c4
safiylm <121391404+safiylm@users.noreply.github.com>
2024-01-07 18:55:52 +0100

    update home page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,7 @@
-﻿using Forum_descussion_ASP.NET_core_mvc.Models;
+﻿using Forum_descussion_ASP.NET_core_mvc.Data;
+using Forum_descussion_ASP.NET_core_mvc.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace Forum_descussion_ASP.NET_core_mvc.Controllers
@@ -7,19 +9,23 @@ namespace Forum_descussion_ASP.NET_core_mvc.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ForumContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+  
+        public HomeController(ILogger<HomeController> logger, ForumContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+  
+        // for display in home page 
+        public async Task<IActionResult> Index()
         {
             ViewData["iduser"] = HttpContext.Session.GetInt32("iduser");
-
-            return View();
+            var forumContext = _context.QuestionModel.Include(q => q.User);
+            return View(await forumContext.ToListAsync());
         }
-
 
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
